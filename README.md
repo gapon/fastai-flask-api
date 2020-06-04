e33bf3491d544f243a221f0e39c218642594e585
gapon <eugenegapon@gmail.com>
2020-06-04 22:23:07 +0300

    Add POST method for predict

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Deploying a fastai model via a REST API with Flask
 
 Run a Flask development server by typing:
-
+```
 $ FLASK_ENV=development FLASK_APP=app.py flask run
+```
--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from fastai.vision import *
+import io
 
 app = Flask(__name__)
 
 path = Path('data')
 defaults.device = torch.device('cpu')
+# Load model
 learn = load_learner(path)
+
+def get_image(image_bytes):
+    return open_image(io.BytesIO(image_bytes)).resize(224)
 
 def get_prediction(img):
     pred_class,pred_idx,outputs = learn.predict(img)
-
     return str(pred_class), str(pred_idx.item()), str(outputs)
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    img = open_image(path/'test.jpg').resize(224)
-    pred_class,pred_idx,outputs = get_prediction(img)
+    if request.method == 'POST':
+        # get the file from the request
+        file = request.files['file']
+        img_bytes = file.read()
+        img = get_image(img_bytes)
+        pred_class,pred_idx,outputs = get_prediction(img)
+        return jsonify({'class_name': pred_class, 'class_id': pred_idx, 'outputs': outputs})
 
-    return jsonify({'class_name': pred_class, 'class_id': pred_idx, 'outputs': outputs})
+
+if __name__ == '__main__':
+    app.run()
